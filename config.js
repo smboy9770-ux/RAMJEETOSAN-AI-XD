549fe0266b11801d8f7025f0c9e7049cb35c4aa0
smboy9770-ux <smboy9770@gmail.com>
2026-03-24 10:35:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     // ===== BOT CORE SETTINGS =====
-    SESSION_ID: process.env.SESSION_ID || "",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "ADEEL-XMD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiS0pucEZ4MldlY25xR0ZSZ0p3MkxzMjFrNnU0UUtXVzRjTmUyNXRNTWJFTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZ0RlT0tyaVVLeUtMR0JMYS9GcGx4N25TMDI3bmlyOHRyYVVJSW5JbllUYz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJNR3BwVFVjazNFSmtYbDEyU0lxVmxCOGljU1d1cTBtdy9FOTRSNW5pUTJNPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJBRGpZKzQ2dkpDYVdkYkg0aWpBM05xazJVNmNyTWVkbGRmZ0RJTjloUlZRPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImNKWHU3d0FvdjFNdkFtbFhJZjJyUWVBWGpxM3NnRGo2dFJlNzRFcm5qR3c9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImZaSjc2bXdjQzc3RU42RUJTUHNrMitNRHJ4aGZhQUxseUFQenNqQ3JHQ1E9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRUlweVB1NEVoUmZyVlA0UGNyTjV2MjN5YjRZekRTbDdLYmVHdmlDd09rWT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWC8zYWh3UjBmeVFoa2ljTXh5SThoNkJxYzNmdmtUOGF0cHQyLzlIYU1YYz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkpZR3B0cUdEQ3VPZlNua0JPOHNOM2N5Qms0M0JPVSt3MlF1cXhMV1lRaWR6OUUrY1VENUc1SjVQUzd6NU9tWGRKaHNQRFdhQzN6MU41ODFKYXVXVWlRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjMyLCJhZHZTZWNyZXRLZXkiOiI4YlJQYjhPNE16RElsOThXVzBObXJhSVZJM21WT1dQUStIV095clR2UFFrPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6ODEzLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6ODEzLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJyZWdpc3RlcmVkIjp0cnVlLCJwYWlyaW5nQ29kZSI6IkZFRU1PME1EIiwibWUiOnsiaWQiOiI5MTc4NjY5Nzg0MDI6MjFAcy53aGF0c2FwcC5uZXQiLCJuYW1lIjoi4oCOIOKAjiDigI4g4oCOIOKAjiDigI4g4oCOIOKAjiDigI4g4oCOICDigI4g4oCOIOKAjiDigI4g4oCOIOKAjiDigI4g4oCOIOKAjiDigI4g4qS58J2Zjc2i8J2emvCdnpvMpPCdmYXwnZ6dzZzwnZ6d8J2eo+KAjtmg4qS4IiwibGlkIjoiMzk2NTU5ODY3NDU0Nzg6MjFAbGlkIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNKMzBnYkFHRUpTQmlNNEdHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiI1QmtZcCtLNkNvNkoxZXZTMktSbnM3Yjd2QU5vVmJvZ2daODVIdURvSVhzPSIsImFjY291bnRTaWduYXR1cmUiOiIwNVVhamg0QXZjSExvVVlRTlF4OXNwRGE1d0RqbEc3ZDdYbjFMbmlJYkV6Y2N3SVJMZE1KKzZHb2E3WVM5bUM0SDFCNzN5MmIxKys3bXQxZ0xCdnZCZz09IiwiZGV2aWNlU2lnbmF0dXJlIjoiS3NaUGFBL3lsYTUxa1JrRXJKNkpVeW5BSUoxTHhENnptRjdlTnR4WlFMdTArQWZHdjUrbXRoVGx6NGlqT0t4aVBXZ1hiNDBHWG5EUDgyTUlucVE0amc9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5MTc4NjY5Nzg0MDI6MjFAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCZVFaR0tmaXVncU9pZFhyMHRpa1o3TzIrN3dEYUZXNklJR2ZPUjdnNkNGNyJ9fV0sInBsYXRmb3JtIjoic21iYSIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0FnSUVnZ04ifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzc0MzIxODI4LCJsYXN0UHJvcEhhc2giOiIzbWwxalMifQ==",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     CHATBOT: getConfig("CHATBOT") || "on", // on/off chat bot 
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "𝐀𝐃𝚵𝚵𝐋-𝐌𝐃",  // Bot's display name
@@ -19,15 +19,15 @@ module.exports = {
     BAILEYS: process.env.BAILEYS || "@whiskeysockets/baileys",  // Bot's BAILEYS
 
     // ===== OWNER & DEVELOPER SETTINGS =====
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "923174838990",  // Owner's WhatsApp number
-    OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "𝐀𝐃𝚵𝚵𝐋-𝐌𝐃",           // Owner's name
-    DEV: process.env.DEV || "923174838990",                     // Developer's contact number
-    DEVELOPER_NUMBER: '923174838990@s.whatsapp.net',            // Developer's WhatsApp ID
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "917866978402",  // Owner's WhatsApp number
+    OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "RAMJEET-XD",           // Owner's name
+    DEV: process.env.DEV || "917866978402",                     // Developer's contact number
+    DEVELOPER_NUMBER: '917866978402@s.whatsapp.net',            // Developer's WhatsApp ID
 
     // ===== AUTO-RESPONSE SETTINGS =====
     AUTO_REPLY: process.env.AUTO_REPLY || "false",              // Enable/disable auto-reply
     AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",// Reply to status updates?
-    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*𝐀𝐃𝚵𝚵𝐋-𝐌𝐃 VIEWED YOUR STATUS 🤖*",  // Status reply message
+    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*RAMJEET-XD VIEWED YOUR STATUS 🤖*",  // Status reply message
     READ_MESSAGE: process.env.READ_MESSAGE || "false",          // Mark messages as read automatically?
     REJECT_MSG: process.env.REJECT_MSG || "*📞 THIS PERSON NOT ALLOWED CALL*",
     // ===== REACTION & STICKER SETTINGS =====
@@ -35,13 +35,13 @@ module.exports = {
     OWNER_REACT: process.env.OWNER_REACT || "false",              // Auto-react to messages?
     CUSTOM_REACT: process.env.CUSTOM_REACT || "false",          // Use custom emoji reactions?
     CUSTOM_REACT_EMOJIS: getConfig("CUSTOM_REACT_EMOJIS") || process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",  // set custom reacts
-    STICKER_NAME: process.env.STICKER_NAME || "𝐀𝐃𝚵𝚵𝐋-𝐌𝐃",     // Sticker pack name
+    STICKER_NAME: process.env.STICKER_NAME || "RAMJEET-XD",     // Sticker pack name
     AUTO_STICKER: process.env.AUTO_STICKER || "false",          // Auto-send stickers?
     // ===== MEDIA & AUTOMATION =====
     AUTO_RECORDING: process.env.AUTO_RECORDING || "false",      // Auto-record voice notes?
     AUTO_TYPING: process.env.AUTO_TYPING || "false",            // Show typing indicator?
     MENTION_REPLY: process.env.MENTION_REPLY || "false",   // reply on mentioned message 
-    MENU_IMAGE_URL: getConfig("MENU_IMAGE_URL") || "https://files.catbox.moe/pf9a6s.jpg",  // Bot's "alive" menu mention image
+    MENU_IMAGE_URL: getConfig("MENU_IMAGE_URL") || "https://files.catbox.moe/9sn87j.jpg",  // Bot's "alive" menu mention image
 
     // ===== SECURITY & ANTI-FEATURES =====
     ANTI_DELETE: process.env.ANTI_DELETE || "true", // true antidelete to recover deleted messages 
@@ -55,8 +55,8 @@ module.exports = {
     PM_BLOCKER: process.env.PM_BLOCKER || "true",
 
     // ===== BOT BEHAVIOR & APPEARANCE =====
-    DESCRIPTION: process.env.DESCRIPTION || "*📌 ᴘᴏᴡᴇʀ ʙʏ ᴍᴀғɪᴀ ᴀᴅᴇᴇʟ*",  // Bot description
-    PUBLIC_MODE: process.env.PUBLIC_MODE || "true",              // Allow public commands?
+    DESCRIPTION: process.env.DESCRIPTION || "*📌 ᴘᴏᴡᴇʀ ʙʏ LORD RAMJEET*",  // Bot description
+    PUBLIC_MODE: process.env.PUBLIC_MODE || "false",              // Allow public commands?
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",        // Show bot as always online?
     AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true", // React to status updates?
     AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true", // VIEW to status updates?
